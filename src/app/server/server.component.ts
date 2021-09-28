22d4e1bee7d2de5b6ecbc97b6cedc1d9616e6884
deyRupak <rupakdey9999@gmail.com>
2021-09-28 12:39:36 +0530

    server : property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,16 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
 
-export class ServerComponent {
+export class ServerComponent implements OnInit{
     serverID: number = 10;
     // serverID = 10 also works!
     serverStatus: string = 'online'
 
     getServerStatus(){
         return this.serverStatus
+    }
+
+
+    allowServer = false
+    constructor() {
+        setTimeout(()=>{
+            this.allowServer = true;
+        }, 6000)
+    }
+
+    ngOnInit(){
+
     }
 }
