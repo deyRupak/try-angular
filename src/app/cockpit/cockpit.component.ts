e33bca9b8357dd2abbb8d38edc13cf90aca5030d
deyRupak <rupakdey9999@gmail.com>
2021-10-03 20:29:13 +0530

    access with viewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,9 +9,11 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   
-  newServerName = ''
-  newServerContent = ''
-  
+  // newServerName = ''
+  // newServerContent = ''
+  @ViewChild('serverContentInput', {static : true}) serverContentInput : ElementRef;
+
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,15 +22,19 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput : HTMLInputElement){
     console.log(nameInput.value)
     this.serverCreated.emit({
+
+      // local ref
       serverName : nameInput.value,
-      serverContent: this.newServerContent
+      
+      // viewChild
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
-  onAddBlueprint(){
+  onAddBlueprint(nameInput : HTMLInputElement){
     this.blueprintCreated.emit({
-      serverName : this.newServerName,
-      serverContent: this.newServerContent
+      serverName : nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
