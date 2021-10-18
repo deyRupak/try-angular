38a3ad5365e6969455f6d14b5385c51365cb564c
deyRupak <rupakdey9999@gmail.com>
2021-10-18 12:16:20 +0530

    add HostListener HostBinding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,11 +1,12 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
-import { workers } from 'cluster';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  //  allows to bind to any property of the element associated
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'
   
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
   // why is 'Renderer' a better approach?
@@ -16,7 +17,25 @@ export class BetterHighlightDirective implements OnInit {
 
   ngOnInit(){
     // element, style, value, flag(optional; eg. !important)
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
   }
 
+
+  // listens to the DOM event on the host element
+  // It also provides a handler method to run when that event occurs
+  @HostListener('mouseenter') mouseover(eventData : Event){
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    
+    // when using HostBinding
+    this.backgroundColor = 'blue'
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData : Event){
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+
+    this.backgroundColor = 'transparent'
+  }
+
+
+  
 }
